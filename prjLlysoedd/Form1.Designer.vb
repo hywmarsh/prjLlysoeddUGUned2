233c357cd771112f5bf2719adf8f6361f0e0b3bd
hywmarsh <hywmarsh@gmail.com>
2016-04-04 21:18:48 +0100

    Highest score but not count back or validation

---
+++ prjLlysoedd/Form1.Designer.vb
@@ -0,0 +1,187 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblDewi = New System.Windows.Forms.Label()
+        Me.txtDewi = New System.Windows.Forms.TextBox()
+        Me.txtDyfrig = New System.Windows.Forms.TextBox()
+        Me.lblDyfrig = New System.Windows.Forms.Label()
+        Me.txtIlltud = New System.Windows.Forms.TextBox()
+        Me.lblIlltud = New System.Windows.Forms.Label()
+        Me.txtTeilo = New System.Windows.Forms.TextBox()
+        Me.lblTeilo = New System.Windows.Forms.Label()
+        Me.btnYchwanegu = New System.Windows.Forms.Button()
+        Me.btnLlysBuddugol = New System.Windows.Forms.Button()
+        Me.lblCanlyniadauDrosDro = New System.Windows.Forms.Label()
+        Me.lblSgorLlysoedd = New System.Windows.Forms.Label()
+        Me.lblLlysBuddugol = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblDewi
+        '
+        Me.lblDewi.AutoSize = True
+        Me.lblDewi.Location = New System.Drawing.Point(21, 45)
+        Me.lblDewi.Name = "lblDewi"
+        Me.lblDewi.Size = New System.Drawing.Size(31, 13)
+        Me.lblDewi.TabIndex = 0
+        Me.lblDewi.Text = "Dewi"
+        '
+        'txtDewi
+        '
+        Me.txtDewi.Location = New System.Drawing.Point(98, 44)
+        Me.txtDewi.Name = "txtDewi"
+        Me.txtDewi.Size = New System.Drawing.Size(100, 20)
+        Me.txtDewi.TabIndex = 1
+        '
+        'txtDyfrig
+        '
+        Me.txtDyfrig.Location = New System.Drawing.Point(98, 70)
+        Me.txtDyfrig.Name = "txtDyfrig"
+        Me.txtDyfrig.Size = New System.Drawing.Size(100, 20)
+        Me.txtDyfrig.TabIndex = 3
+        '
+        'lblDyfrig
+        '
+        Me.lblDyfrig.AutoSize = True
+        Me.lblDyfrig.Location = New System.Drawing.Point(21, 70)
+        Me.lblDyfrig.Name = "lblDyfrig"
+        Me.lblDyfrig.Size = New System.Drawing.Size(34, 13)
+        Me.lblDyfrig.TabIndex = 2
+        Me.lblDyfrig.Text = "Dyfrig"
+        '
+        'txtIlltud
+        '
+        Me.txtIlltud.Location = New System.Drawing.Point(98, 96)
+        Me.txtIlltud.Name = "txtIlltud"
+        Me.txtIlltud.Size = New System.Drawing.Size(100, 20)
+        Me.txtIlltud.TabIndex = 5
+        '
+        'lblIlltud
+        '
+        Me.lblIlltud.AutoSize = True
+        Me.lblIlltud.Location = New System.Drawing.Point(21, 96)
+        Me.lblIlltud.Name = "lblIlltud"
+        Me.lblIlltud.Size = New System.Drawing.Size(29, 13)
+        Me.lblIlltud.TabIndex = 4
+        Me.lblIlltud.Text = "Illtud"
+        '
+        'txtTeilo
+        '
+        Me.txtTeilo.Location = New System.Drawing.Point(98, 122)
+        Me.txtTeilo.Name = "txtTeilo"
+        Me.txtTeilo.Size = New System.Drawing.Size(100, 20)
+        Me.txtTeilo.TabIndex = 7
+        '
+        'lblTeilo
+        '
+        Me.lblTeilo.AutoSize = True
+        Me.lblTeilo.Location = New System.Drawing.Point(21, 123)
+        Me.lblTeilo.Name = "lblTeilo"
+        Me.lblTeilo.Size = New System.Drawing.Size(30, 13)
+        Me.lblTeilo.TabIndex = 6
+        Me.lblTeilo.Text = "Teilo"
+        '
+        'btnYchwanegu
+        '
+        Me.btnYchwanegu.Location = New System.Drawing.Point(293, 38)
+        Me.btnYchwanegu.Name = "btnYchwanegu"
+        Me.btnYchwanegu.Size = New System.Drawing.Size(81, 32)
+        Me.btnYchwanegu.TabIndex = 8
+        Me.btnYchwanegu.Text = "Ychwanegu"
+        Me.btnYchwanegu.UseVisualStyleBackColor = True
+        '
+        'btnLlysBuddugol
+        '
+        Me.btnLlysBuddugol.Location = New System.Drawing.Point(293, 96)
+        Me.btnLlysBuddugol.Name = "btnLlysBuddugol"
+        Me.btnLlysBuddugol.Size = New System.Drawing.Size(80, 40)
+        Me.btnLlysBuddugol.TabIndex = 9
+        Me.btnLlysBuddugol.Text = "Llys Buddugol"
+        Me.btnLlysBuddugol.UseVisualStyleBackColor = True
+        '
+        'lblCanlyniadauDrosDro
+        '
+        Me.lblCanlyniadauDrosDro.AutoSize = True
+        Me.lblCanlyniadauDrosDro.Location = New System.Drawing.Point(32, 203)
+        Me.lblCanlyniadauDrosDro.Name = "lblCanlyniadauDrosDro"
+        Me.lblCanlyniadauDrosDro.Size = New System.Drawing.Size(39, 13)
+        Me.lblCanlyniadauDrosDro.TabIndex = 10
+        Me.lblCanlyniadauDrosDro.Text = "Label1"
+        '
+        'lblSgorLlysoedd
+        '
+        Me.lblSgorLlysoedd.AutoSize = True
+        Me.lblSgorLlysoedd.Location = New System.Drawing.Point(127, 203)
+        Me.lblSgorLlysoedd.Name = "lblSgorLlysoedd"
+        Me.lblSgorLlysoedd.Size = New System.Drawing.Size(39, 13)
+        Me.lblSgorLlysoedd.TabIndex = 11
+        Me.lblSgorLlysoedd.Text = "Label1"
+        '
+        'lblLlysBuddugol
+        '
+        Me.lblLlysBuddugol.AutoSize = True
+        Me.lblLlysBuddugol.Location = New System.Drawing.Point(272, 203)
+        Me.lblLlysBuddugol.Name = "lblLlysBuddugol"
+        Me.lblLlysBuddugol.Size = New System.Drawing.Size(39, 13)
+        Me.lblLlysBuddugol.TabIndex = 12
+        Me.lblLlysBuddugol.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(456, 342)
+        Me.Controls.Add(Me.lblLlysBuddugol)
+        Me.Controls.Add(Me.lblSgorLlysoedd)
+        Me.Controls.Add(Me.lblCanlyniadauDrosDro)
+        Me.Controls.Add(Me.btnLlysBuddugol)
+        Me.Controls.Add(Me.btnYchwanegu)
+        Me.Controls.Add(Me.txtTeilo)
+        Me.Controls.Add(Me.lblTeilo)
+        Me.Controls.Add(Me.txtIlltud)
+        Me.Controls.Add(Me.lblIlltud)
+        Me.Controls.Add(Me.txtDyfrig)
+        Me.Controls.Add(Me.lblDyfrig)
+        Me.Controls.Add(Me.txtDewi)
+        Me.Controls.Add(Me.lblDewi)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblDewi As System.Windows.Forms.Label
+    Friend WithEvents txtDewi As System.Windows.Forms.TextBox
+    Friend WithEvents txtDyfrig As System.Windows.Forms.TextBox
+    Friend WithEvents lblDyfrig As System.Windows.Forms.Label
+    Friend WithEvents txtIlltud As System.Windows.Forms.TextBox
+    Friend WithEvents lblIlltud As System.Windows.Forms.Label
+    Friend WithEvents txtTeilo As System.Windows.Forms.TextBox
+    Friend WithEvents lblTeilo As System.Windows.Forms.Label
+    Friend WithEvents btnYchwanegu As System.Windows.Forms.Button
+    Friend WithEvents btnLlysBuddugol As System.Windows.Forms.Button
+    Friend WithEvents lblCanlyniadauDrosDro As System.Windows.Forms.Label
+    Friend WithEvents lblSgorLlysoedd As System.Windows.Forms.Label
+    Friend WithEvents lblLlysBuddugol As System.Windows.Forms.Label
+
+End Class
---
+++ prjLlysoedd/Form1.vb
@@ -0,0 +1,132 @@
+﻿Public Class Form1
+    'Gweerthoedd eang sy'n cael eu defnyddio mewn mwy nag un modiwl
+    Const NiferLlysoedd = 4 'nifer o lysoedd
+    Const NiferSafleoedd = 4 'nifer posib o safleoedd ym mhod cystadleuaeth
+    Dim SafleoeddLlys(NiferLlysoedd, NiferSafleoedd) As Integer 'Cyfrif y nifer o safleoedd cafodd pob llys
+    Dim CyfanswmSgorLlys(NiferLlysoedd) As Integer 'Cyfanswm sgor pob llys
+    Dim EnwauLlysoedd(NiferLlysoedd) As String 'Storio enwau llysoed ynnhrefn yr wyddor
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim llys As Integer 'rheoli dolen ar gyfer pob llys
+        Dim safle As Integer 'rheoli dolen safle
+        'Rhoi enwau i bob llys
+        EnwauLlysoedd(0) = "Dewi"
+        EnwauLlysoedd(1) = "Dyfrig"
+        EnwauLlysoedd(2) = "Illtud"
+        EnwauLlysoedd(3) = "Teilo"
+
+        'Gwerthcychwynol o 0 ar gyfer y cyfanswm sgor a safleoedd y llysoedd
+        For llys = 0 To NiferLlysoedd - 1
+            'Gosod cyfanswm sgor y llys i 0
+            CyfanswmSgorLlys(llys) = 0
+            'Gosod y cyfrif o safleoedd y llys i 0
+            For safle = 0 To NiferSafleoedd - 1
+                SafleoeddLlys(llys, safle) = 0
+            Next
+        Next
+        'Enwau Llysoedd
+
+        arddangosDrosDro(CyfanswmSgorLlys, SafleoeddLlys)
+    End Sub
+
+    Private Sub arddangosDrosDro(ByVal Cyfanswm As Object, ByVal Safleoedd As Object)
+        Dim llys As Integer 'rheoli dolen ar gyfer pob llys
+        Dim safle As Integer 'rheoli dolen safle
+
+        lblCanlyniadauDrosDro.Text = "Sgor y Llysoedd" & vbCrLf
+        For llys = 0 To NiferLlysoedd - 1
+            'Allbynu cyfanswm sgor y llys 
+            lblCanlyniadauDrosDro.Text = lblCanlyniadauDrosDro.Text & EnwauLlysoedd(llys) & ": " & Cyfanswm(llys) & vbCrLf
+        Next
+
+        'Llbynnu y cyfrif o safleoedd y llys
+        lblCanlyniadauDrosDro.Text = lblCanlyniadauDrosDro.Text & "Safleoedd" & vbCrLf
+        For llys = 0 To NiferLlysoedd - 1
+            lblCanlyniadauDrosDro.Text = lblCanlyniadauDrosDro.Text & EnwauLlysoedd(llys) & ": "
+            For safle = 0 To NiferSafleoedd - 1
+                lblCanlyniadauDrosDro.Text = lblCanlyniadauDrosDro.Text & Safleoedd(llys, safle) & "  "
+            Next
+            lblCanlyniadauDrosDro.Text = lblCanlyniadauDrosDro.Text & vbCrLf
+        Next
+    End Sub
+
+    Private Sub btnYchwanegu_Click(sender As Object, e As EventArgs) Handles btnYchwanegu.Click
+        'Mewnbynnu ac ychwanegu data
+        Dim canlyniadau(NiferSafleoedd) As Integer
+        Dim llys As Integer 'dolen llys
+        'Dim Dewi As Integer 'Safle Dewi
+        'Dim Dyfrig As Integer 'Safle Dyfrig
+        'Dim Illtud As Integer 'Safle Illtud
+        'Dim Teilo As Integer 'Safle Teilo
+        'darllen y safleoedd
+
+
+        canlyniadau(0) = txtDewi.Text
+        canlyniadau(1) = txtDyfrig.Text
+        canlyniadau(2) = txtIlltud.Text
+        canlyniadau(3) = txtTeilo.Text
+
+        'Diweddaru'r sgor a safleoedd
+        For llys = 0 To NiferLlysoedd - 1
+            'Ychwanegu at y cyfanswm
+            CyfanswmSgorLlys(llys) = CyfanswmSgorLlys(llys) + canlyniadau(llys)
+            'Ychwanegu at y safle y llys a'r sgor yny blwch testun yw'r safle i adio 1 ato
+            SafleoeddLlys(llys, canlyniadau(llys) - 1) = SafleoeddLlys(llys, canlyniadau(llys) - 1) + 1
+        Next
+        arddangosDrosDro(CyfanswmSgorLlys, SafleoeddLlys)
+    End Sub
+
+    Private Sub btnLlysBuddugol_Click(sender As Object, e As EventArgs) Handles btnLlysBuddugol.Click
+        'Darganfod y llys buddugol
+        Dim llysGydaSorIsaf(NiferLlysoedd) 'Rhif(au) y Llys(oedd) gyda'r sgor isaf
+        Dim NiferGydaSgorIsaf As Integer = 0
+      
+        'Arddangos sgor pob llys
+        arddangosSgor(CyfanswmSgorLlys)
+
+        'Darganfod ennillydd
+        'Darganfod y sgor isaf (mwyaf o safleoedd isel)
+        Dim SgorIsaf As Integer = 9999
+        Dim llys As Integer 'dolen llys
+        For llys = 0 To NiferLlysoedd - 1
+            'Cymharu cyfanswm sgor pob llys
+            If CyfanswmSgorLlys(llys) < SgorIsaf Then ' Dyma'r sgor isaf mor belled
+                'Newid y sgor iasf i sgor y llys yma
+                SgorIsaf = CyfanswmSgorLlys(llys)
+            End If
+        Next
+        'darganfod pa llys sydd a'r sgor isaf
+        For llys = 0 To NiferLlysoedd - 1
+            'Cymharu cyfanswm sgor pob llys
+            If CyfanswmSgorLlys(llys) = SgorIsaf Then ' Llys yma'r sgor isaf
+                'Ychwanegu rhif y llys i arae sy'n storio y llysoedd sydd a'r sgor isaf
+                llysGydaSorIsaf(NiferGydaSgorIsaf) = llys
+                'Ychwanegu un at y nifer gyda sgor isaf
+                NiferGydaSgorIsaf = NiferGydaSgorIsaf + 1
+            End If
+        Next
+        'Darganfod y nifer o ennillwyr
+        If NiferGydaSgorIsaf = 1 Then ' Enillydd clir
+            lblLlysBuddugol.Text = " Y llys buddugol yw " & EnwauLlysoedd(llysGydaSorIsaf(0))
+        Else ' mwy nag un llys gyda'r un sgor
+            Cyfrifnol(llysGydaSorIsaf, NiferGydaSgorIsaf)
+        End If
+    End Sub
+
+    Private Sub arddangosSgor(ByVal Cyfanswm As Object)
+        'Arddangos cyfanswm sgor pob llys
+
+        Dim llys As Integer 'rheoli dolen ar gyfer pob llys
+
+        lblSgorLlysoedd.Text = "Sgor y Llysoedd" & vbCrLf
+        For llys = 0 To NiferLlysoedd - 1
+            'Allbynu cyfanswm sgor y llys 
+            lblSgorLlysoedd.Text = lblSgorLlysoedd.Text & EnwauLlysoedd(llys) & ": " & Cyfanswm(llys) & vbCrLf
+        Next
+    End Sub
+
+    Private Sub Cyfrifnol(ByVal llysoeddCyfartal As Object, ByVal Nifer As Integer)
+        'Nifer gyda'r un sgor
+
+    End Sub
+End Class
